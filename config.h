0e45909e6655ad7b9eb2e2d4c2f6a83bf6698f96
Ethan Chen <24852017+Ethan707@users.noreply.github.com>
2023-06-05 02:09:49 -0600

    Init Change

--- config.h
+++ config.h
@@ -5,13 +5,15 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "MesloLGLDZNerdFont:size=16" };
+static const char dmenufont[]       = "MesloLGLDZNerdFont:size=16";
+static unsigned int baralpha        = 127;
+static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#FF9E2C";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -39,8 +41,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[T]=",      tile },    /* first entry is default */
+	{ "<F>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
